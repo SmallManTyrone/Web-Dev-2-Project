8de70267b55dde4c840580b4d8169f2fc5de7b54
Tyson La <Tysonala@hotmail.com>
2023-11-08 00:35:13 -0600

    4.5 fixed a bug

--- Web 2 project/login.php
+++ Web 2 project/login.php
@@ -1,68 +1,4 @@
 <?php
-
-
-$servername = "localhost"; // Replace with your database server
-$username = "serveruser"; // Replace with your database username
-$password = "gorgonzola7!"; // Replace with your database password
-$dbname = "serverside"; // Replace with your database name
-
-// Create a database connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-$error_message = ''; // Initialize the error message
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['login'])) {
-    $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
-    $password = $_POST['password'];
-
-    // Check the 'admins' table for admin login
-    $admin_sql = "SELECT * FROM admins WHERE Username = ?";
-    $stmt = $conn->prepare($admin_sql);
-    $stmt->bind_param('s', $username);
-    $stmt->execute();
-    $admin_result = $stmt->get_result()->fetch_assoc();
-
-    // Try authenticating as an admin
-    if ($admin_result && $password === $admin_result['Password']) {
-        $_SESSION['admin_id'] = $admin_result['AdminID'];
-        $_SESSION['is_admin'] = true;
-        $_SESSION['username'] = $username;
-        $_SESSION['login_success'] = true;
-
-        // Redirect to the admin dashboard
-        header("Location: index.php");
-        exit();
-    }
-
-    // If the username is not in the 'admins' table, it's a regular user
-    $user_sql = "SELECT * FROM user WHERE Username = ?";
-    $stmt = $conn->prepare($user_sql);
-    $stmt->bind_param('s', $username);
-    $stmt->execute();
-    $result = $stmt->get_result()->fetch_assoc();
-
-    // If admin authentication fails, try authenticating as a regular user
-    if ($result && password_verify($password, $result['Password'])) {
-        $_SESSION['user_id'] = $result['UserID'];
-        $_SESSION['is_admin'] = false; // Marking as a regular user
-        $_SESSION['username'] = $username;
-        $_SESSION['login_success'] = true;
-
-        // Redirect to the user dashboard
-        header("Location: index.php");
-        exit();
-    } else {
-        $error_message = "Login failed. Check your credentials.";
-        header("Location: login.php?error=" . urlencode($error_message)); // Redirect with error message
-        exit();
-    }
-}
-
 require('authenticate.php');
 
 // Check if a user is already logged in
@@ -77,7 +13,27 @@ if (isAdminLoggedIn()) {
 }
 
 $error_message = ''; // Initialize the error message
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['login'])) {
+    $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
+    $password = $_POST['password'];
+
+    // Try authenticating as an admin
+    if (authenticateAdmin($db, $username, $password)) {
+        header("Location: index.php"); // Redirect to admin dashboard
+        exit();
+    }
+
+    // If admin authentication fails, try authenticating as a regular user
+    if (authenticateUser($db, $username, $password)) {
+        header("Location: index.php"); // Redirect to user dashboard
+        exit();
+    }
+
+    $error_message = "Login failed. Check your credentials.";
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -101,7 +57,7 @@ $error_message = ''; // Initialize the error message
         echo '<p><a href="registration.php">Register</a></p>';
     }
     ?>
-    <form action="login.php" method="post" onsubmit="showLoginSuccess()">
+    <form action="login_process.php" method="post" onsubmit="showLoginSuccess()">
         <label for="username">Username:</label>
         <input type="text" id="username" name="username" required>
 
---
+++ Web 2 project/login_process.php
@@ -0,0 +1,64 @@
+<?php
+session_start();
+require('connect.php'); // Include your database connection script
+
+$servername = "localhost"; // Replace with your database server
+$username = "serveruser"; // Replace with your database username
+$password = "gorgonzola7!"; // Replace with your database password
+$dbname = "serverside"; // Replace with your database name
+
+// Create a database connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$error_message = ''; // Initialize the error message
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['login'])) {
+    $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
+    $password = $_POST['password'];
+
+    // Check the 'admins' table for admin login
+    $admin_sql = "SELECT * FROM admins WHERE Username = ?";
+    $stmt = $conn->prepare($admin_sql);
+    $stmt->bind_param('s', $username);
+    $stmt->execute();
+    $admin_result = $stmt->get_result()->fetch_assoc();
+    
+    if ($admin_result && $password === $admin_result['Password']) {
+        $_SESSION['admin_id'] = $admin_result['AdminID'];
+        $_SESSION['is_admin'] = true;
+        $_SESSION['username'] = $username;
+        $_SESSION['login_success'] = true;
+
+        // Redirect to the admin dashboard
+        header("Location: index.php");
+        exit();
+    }
+
+    // If the username is not in the 'admins' table, it's a regular user
+    $user_sql = "SELECT * FROM user WHERE Username = ?";
+    $stmt = $conn->prepare($user_sql);
+    $stmt->bind_param('s', $username);
+    $stmt->execute();
+    $result = $stmt->get_result()->fetch_assoc();
+
+    if ($result && password_verify($password, $result['Password'])) {
+        $_SESSION['user_id'] = $result['UserID'];
+        $_SESSION['is_admin'] = false; // Marking as a regular user
+        $_SESSION['username'] = $username;
+        $_SESSION['login_success'] = true;
+    
+        // Redirect to the user dashboard
+        header("Location: index.php");
+        exit();
+    } else {
+        $error_message = "Login failed. Check your credentials.";
+        header("Location: login.php?error=" . urlencode($error_message)); // Redirect with error message
+        exit();
+    }
+}
+?>
